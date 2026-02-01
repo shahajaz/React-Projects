26c978034c7560331aa8fda04557331115e99032
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-02-01 21:27:38 +0530

    Components Initialisation

--- E-Commerce/src/App.jsx
+++ E-Commerce/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import {Routers,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[] lg:px-[9vw]'>
       
+      <Routers>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/collections' element={<Collection />} />
+        <Route path='/products/:productId' element={<Product />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/order/:orderId' element={<Orders />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routers>
     </div>
   )
 }
