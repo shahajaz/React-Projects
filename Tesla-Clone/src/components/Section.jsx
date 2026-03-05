52379864cc6598359a2c8798553c3e4934f5d24f
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-03-05 21:37:09 +0530

    Home Buttons

---
+++ Tesla-Clone/src/components/Section.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Section = () => {
+    return (
+        <div className="Wrap">
+            <div className="ItemText">
+                <h1>Model S</h1>
+                <p>Order Online for Touch-less delivery</p>
+            </div>
+
+            <div className="ButtonGrp">
+                <div className="OrderBtn">Order Now</div>
+
+                <div className="ViewBtn"> View Inventory</div>
+            </div>
+        </div>
+    )
+}
+
+export default Section;
