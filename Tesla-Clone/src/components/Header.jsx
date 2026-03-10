aae60099b667a094230438026277c4c546542d05
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-03-10 20:29:16 +0530

    SideBar

--- Tesla-Clone/src/components/Header.jsx
+++ Tesla-Clone/src/components/Header.jsx
@@ -1,29 +1,92 @@
-import React from 'react'
+// import React from 'react'
 
+// const Header = () => {
+//     return (
+//     <div className='header-container'>
+//         <div className="logo">
+//             <img src="/images/logo.png" alt="Logo"/>
+//         </div>
+
+//         <div className="Menu">
+//             <a href="#">Vehicles</a>
+//             <a href="#">Energy</a>
+//             <a href="#">Charging</a>
+//             <a href="#">Discover</a>
+//             <a href="#">Shop</a>
+//             <a href="#">Support</a>
+//         </div>
+
+//         <div class="header-right">
+//             <button class="account">Account</button>
+
+//             <div className="custom-menu"><i class="fa-solid fa-align-justify"></i>
+//                 <div className="custom-Nav">
+//                     <i className="fa-solid fa-x"></i>
+//                     <a href="#">Vehicles</a>
+//                     <a href="#">Energy</a>
+//                     <a href="#">Charging</a>
+//                     <a href="#">Discover</a>
+//                     <a href="#">Shop</a>
+//                     <a href="#">Support</a>
+//                 </div>
+//             </div>
+//         </div>
+
+//     </div>
+//     )
+// }
+
+// export default Header;
+
+import React, { useState } from "react";
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
     return (
-    <div className='header-container'>
+    <div className="header-container">
+        
         <div className="logo">
-            <img src="/images/logo.png" alt="Logo"/>
-        </div>
+            <img src="/images/logo.png" alt="logo"/>
+            </div>
+            
+            <div className={`Menu ${menuOpen ? "hide-menu" : ""}`}>
+                <a href="#">Vehicles</a>
+                <a href="#">Energy</a>
+                <a href="#">Charging</a>
+                <a href="#">Discover</a>
+                <a href="#">Shop</a>
+                <a href="#">Support</a>
+            </div>
+            
+            <div className="header-right">
+                <button className="account">Account</button>
+                
+                <i 
+                    className="fa-solid fa-align-justify menu-icon"
+                    onClick={() => setMenuOpen(true)}
+                ></i>
+                
+            </div>
 
-        <div className="Menu">
-            <a href="#">Vehicles</a>
-            <a href="#">Energy</a>
-            <a href="#">Charging</a>
-            <a href="#">Discover</a>
-            <a href="#">Shop</a>
-            <a href="#">Support</a>
-        </div>
+      {/* Sidebar */}
+      <div className={`custom-Nav ${menuOpen ? "active" : ""}`}>
 
-        <div class="header-right">
-            <button class="account">Account</button>
+        <i 
+          className="fa-solid fa-x close-icon"
+          onClick={() => setMenuOpen(false)}
+        ></i>
 
-            <div className="custom-menu"><i class="fa-solid fa-align-justify"></i></div>
-        </div>
+        <a href="#">Vehicles</a>
+        <a href="#">Energy</a>
+        <a href="#">Charging</a>
+        <a href="#">Discover</a>
+        <a href="#">Shop</a>
+        <a href="#">Support</a>
+
+      </div>
 
     </div>
-    )
-}
+  );
+};
 
 export default Header;
