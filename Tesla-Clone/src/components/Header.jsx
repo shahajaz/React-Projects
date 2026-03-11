23a9d1a5ac8266e4ee6161290450dd649fffcf97
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-03-11 22:59:56 +0530

    sidebar redesign

--- Tesla-Clone/src/components/Header.jsx
+++ Tesla-Clone/src/components/Header.jsx
@@ -1,43 +1,3 @@
-// import React from 'react'
-
-// const Header = () => {
-//     return (
-//     <div className='header-container'>
-//         <div className="logo">
-//             <img src="/images/logo.png" alt="Logo"/>
-//         </div>
-
-//         <div className="Menu">
-//             <a href="#">Vehicles</a>
-//             <a href="#">Energy</a>
-//             <a href="#">Charging</a>
-//             <a href="#">Discover</a>
-//             <a href="#">Shop</a>
-//             <a href="#">Support</a>
-//         </div>
-
-//         <div class="header-right">
-//             <button class="account">Account</button>
-
-//             <div className="custom-menu"><i class="fa-solid fa-align-justify"></i>
-//                 <div className="custom-Nav">
-//                     <i className="fa-solid fa-x"></i>
-//                     <a href="#">Vehicles</a>
-//                     <a href="#">Energy</a>
-//                     <a href="#">Charging</a>
-//                     <a href="#">Discover</a>
-//                     <a href="#">Shop</a>
-//                     <a href="#">Support</a>
-//                 </div>
-//             </div>
-//         </div>
-
-//     </div>
-//     )
-// }
-
-// export default Header;
-
 import React, { useState } from "react";
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -62,7 +22,7 @@ const Header = () => {
                 <button className="account">Account</button>
                 
                 <i 
-                    className="fa-solid fa-align-justify menu-icon"
+                    className="fa-solid fa-align-justify icon-btn"
                     onClick={() => setMenuOpen(true)}
                 ></i>
                 
@@ -72,16 +32,22 @@ const Header = () => {
       <div className={`custom-Nav ${menuOpen ? "active" : ""}`}>
 
         <i 
-          className="fa-solid fa-x close-icon"
+          className="fa-solid fa-x close-icon icon-btn"
           onClick={() => setMenuOpen(false)}
         ></i>
 
         <a href="#">Vehicles</a>
         <a href="#">Energy</a>
         <a href="#">Charging</a>
+        <a href="#">Cyber Trucks</a>
+        <a href="#">Solar Energy</a>
         <a href="#">Discover</a>
         <a href="#">Shop</a>
         <a href="#">Support</a>
+
+        <button className="settingBtn">
+          <i className="fa-solid fa-gear"></i>
+        </button>
 
       </div>
 
