32203467b7a733c112b5a5c70f022acc0d3fc3c0
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-03-12 22:36:42 +0530

    Personal Portfolio Setup

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-400 text-white text-8xl">
+      Tailwind Working 🚀
+    </div>
+  )
+}
+
+export default App
