21fdace396016918d2728af36a959a490b65c929
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-03-16 22:07:59 +0530

    Nav Logo design

--- Portfolio/src/Components/Navbar.jsx
+++ Portfolio/src/Components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from "react";
+import Logo from "../assets/logo.png";
 
-const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Navbar(){
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [visible, setVisible] = useState(true);
+
+  return(
+    <>
+      <nav
+        className={`fixed top-0 left-0 w-full flex items-center px-6 py-4 z-50 
+        transition-transform duration-300 
+        ${visible ? "translate-y-0" : "-translate-y-full"}`} >
+        
+        <div className="flex items-center gap-0.5 cursor-pointer group">
+          <img src={Logo} alt="logo" className="w-16 h-16 object-contain transition-transform duration-300 group-hover:scale-110"/>
+          
+          <div className="text-4xl font-bold bg-gradient-to-r from-[#ff00af] to-[#ffcc00] bg-clip-text text-transparent">Ajaz</div>
+        </div>
+
+      </nav>
+
+      {menuOpen && <OverlayMenu/>}
+    </>
+  );
 }
-
-export default Navbar;
